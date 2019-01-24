47793140cddbdfdac8308fc83c8a0eb1e3ebd8cc
Rutuja Rajesh <rutujarajesh23@gmail.com>
2019-01-24 10:08:52 +0530

    Add files via upload

---
+++ Fork Python/Tracks/Module 1 (Python Basics)/python-io/Start_Coding_Python.py
@@ -0,0 +1,44 @@
+#Start Coding - Python
+"""
+When learning a new language, we first learn to output some message. Here, we'll start with the famous 
+"Hello World" message. Now, here you are given a function to complete. Don't worry about the ins and 
+outs of functions, just add the command (print ("Hello World")) to print Hello World.
+
+# Input Format :
+No input
+
+# Output format :
+Hello World
+
+# User Task:
+Your task is to complete the function print_fun() to print hello world.
+"""
+
+#solution.py
+{
+
+#Initial Template for Python 3
+
+//Position this line where user code will be pasted.
+
+def main():
+    
+    print_fun()
+
+if __name__ == '__main__':
+   
+    main()
+
+}
+
+
+''' Please note that it's Function problem i.e.
+you need to write your solution 
+in the form of Function(s) only.
+Driver Code to 
+call/invoke your function is mentioned above. 
+'''
+
+def print_fun():
+    
+    print("Hello World")
