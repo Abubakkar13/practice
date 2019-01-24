304cef3b108511f56425df7ad5bf8cda9e1a63ed
Rutuja Rajesh <rutujarajesh23@gmail.com>
2019-01-24 15:09:55 +0530

    Add files via upload

---
+++ Fork Python/Tracks/Module 1 (Python Basics)/python-io/9_Comment_in_Python.py
@@ -0,0 +1,24 @@
+#Comment in Python
+"""
+You have learnt about input and output in Python. One of the pivotal thing in the world of programming is comment which is used by developers to describe their code or to preclude compiler from compiling the line(s) which they don't want to. In this section, we will learn how to comment any line in Python.
+
+Given two print statement, the task is to comment the first one, leave next as it is and comment next three (multiple line comment) such that only required output will be generated.
+
+Hint:
+#: is used to comment a single line.
+''' ''' : is used to comment multiple lines.
+
+#Input:
+None
+
+#Output:
+Geeks For Geeks
+
+#Example:
+After commenting the required line, Geeks For Geeks is printed, which is required output.
+"""
+
+#solution.py
+def print_func():
+
+    print ("Geeks For Geeks")
