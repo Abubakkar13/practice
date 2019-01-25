71566a96cd08e138e918f47d84cc0a8a36bb3e14
Rutuja Rajesh <rutujarajesh23@gmail.com>
2019-01-25 10:24:41 +0530

    Add files via upload

---
+++ Fork Python/Tracks/Module 1 (Python Basics)/python-operators/3_Logical_Operators_Python.py
@@ -0,0 +1,41 @@
+#Logical Operators - Python
+"""
+Logical operators are and, or, not. They are used in condition checking.
+Example: a and b checks if both a and b are True. First a is checked then b.
+a or b checks if either of a or b is True. If one is True; it doesn't check for other.
+not a complements the boolean value of a
+Note: 0 and empty string are False and all other values are True.
+
+In this question, you will take two integers as input and print out the output of various logical operations between them.
+
+#Input Format:
+The first line of testcase contains T denoting the number of testcases. T testcases follow. Each testcase contains 2 lines of input.
+
+#Output Format:
+For each testcase, in a new line, Output the answer.
+
+#Your Task:
+Your task is to complete the function logical().
+
+#Constraints:
+1 <= T <= 100
+
+#Example:
+Input:
+1
+5
+6
+Output:
+6
+5
+False
+"""
+
+#solution.py
+def logical(a,b):
+
+    print(a and b) ## do a and b
+
+    print(a or b) ## do a or b
+
+    print(not a) ## do not a
