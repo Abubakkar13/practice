87a11f88cf6ff978c22c73df55452d0ce4bd86cd
Rutuja Rajesh <rutujarajesh23@gmail.com>
2019-01-28 11:54:16 +0530

    Add files via upload

---
+++ Fork Python/Tracks/Module 3 (strings)/python-strings/7_Regular_Expressions_2_Python.py
@@ -0,0 +1,45 @@
+#Regular Expressions 2 - Python
+"""
+In this question, we'll use Regex to validate a password. You will be provided a string str which you have to validate.
+
+The validation rules are as follows:
+
+The string is valid only if it starts with lowercase characters, followed by special characters !@#$% followed by numbers.
+In any other case the string is not valid.
+#Input Format:
+The first line of input contains T denoting the number of testcases. T testcases follow. Each testcase contains one line of input containing str.
+
+#Output Format:
+For each testcase, in a new line, print True or False.
+
+#Your Task:
+This is a function problem. Do not take input. Complete the function validate that takes str as input.
+
+#Constraints:
+1 <= T <= 100
+
+#Example:
+Input:
+1
+asdsab@!@234
+Output:
+True
+
+#Explanation:
+Testcase1: The string is valid as characters are followed by special case characters which are then followed by numbers.
+"""
+
+#solution.py
+def validate(str):
+
+    pat= r"[a-z]+[!@#$%]+[1-9]+"
+
+    match=re.search(pat,str)
+
+    if(match):
+
+        return True
+
+    else:
+
+        return False
