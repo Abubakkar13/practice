045c8479446448e26974b4ae5e7af6c2266e3f65
Rutuja Rajesh <rutujarajesh23@gmail.com>
2019-01-31 14:15:07 +0530

    Add files via upload

---
+++ Fork Python/Tracks/Module 4 (Classes and Functions)/python-function/3_Variable_Arguments_Python.py
@@ -0,0 +1,30 @@
+#Variable Arguments - Python
+"""
+Python functions allow us to take variable arguments. The parameter that takes variable argument has a * as prefix.
+
+In this question we will summ the elements taken as variable arguments and print the result.
+
+#Input Fomat:
+The first line of input contains T denoting the number of testcases. T testcases follow. Each testcase contains 1 line of input containing n.
+
+#Output Format:
+For each testcase, in a new line, print the sum of n with elements of variable argument.
+
+#Your Task:
+This is a function problem. You don't need to take any input. Just complete the function multivar.
+
+#Constraints:
+1 <= T <= 100
+
+#Examples:
+Input:
+1
+6
+Output:
+28
+"""
+
+#solution.py
+def multivar(a, *var):
+ 
+    print(a+sum(var))
