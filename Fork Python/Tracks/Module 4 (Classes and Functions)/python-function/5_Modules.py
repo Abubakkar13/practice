591be56d75334dd609b3f4963fb891c80fb06962
Rutuja Rajesh <rutujarajesh23@gmail.com>
2019-02-02 21:41:19 +0530

    Add files via upload

---
+++ Fork Python/Tracks/Module 4 (Classes and Functions)/python-function/5_Modules.py
@@ -0,0 +1,32 @@
+#Modules
+"""
+As you would already know, Modules refer to files containing already compiled Python statements and definitions. This allow us to break down large number of code lines into group of manageable files.
+Lets play by importing some modules in our code.
+
+You are required to print values of pi constant and e constant in math library.
+
+#Input:
+No input
+
+#Output:
+pi = 3.141592653589793
+e = 2.718281828459045
+
+#Your Task:
+This is a function problem. You don't need to take any input. Complete math_func()
+"""
+
+#solution.py
+import math
+
+#User function Template for python3
+
+def math_func():
+
+    #import math module and print pi and e
+
+
+    print("pi =",math.pi)
+
+    print("e =",math.e)
+
